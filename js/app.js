2cdbaf0a364412e0986531cc394977a8b22ec4ec
Noam Rosenthal <noam@webkit.org>
2021-12-07 10:19:27 +0200

    A vanilla implementation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,67 @@
-(function (window) {
-	'use strict';
+import createTaskListModel from './model.js';
 
-	// Your starting point. Enjoy the ride!
+const app = document.querySelector('.todoapp');
+const list = app.querySelector('.todo-list');
+const formValues = form => Object.fromEntries(new FormData(form));
+const updateFilterFromHash = () => { app.dataset.filter = location.hash.substr(2); }
+window.addEventListener('hashchange', updateFilterFromHash);
+updateFilterFromHash();
+const item = key => list.querySelector(`li#task-${key}`);
+const mainForm = document.forms.main;
+const update = (key, value, element = item(key), form = element.querySelector('form')) => {
+	element.classList.toggle('completed', !!value.completed);
+	form.elements.completed.checked = !!value.completed;
+	element.querySelector('label').innerHTML = form.elements.title.value = value.title;
+	updateCount();
+}
 
-})(window);
+const updateEditing = e =>
+	list.querySelectorAll('.task').forEach(t => t.classList.toggle('editing', t === e));
+
+function updateCount() {
+	const count = app.dataset.activeCount = app.querySelectorAll('.task:not(.completed)').length;
+	app.dataset.completedCount = app.querySelectorAll('.task.completed').length;
+	app.querySelector('.todo-count').innerHTML = `<strong>${count}</strong> item${count === 1 ? '' : 's'} left`;
+}
+
+const model = await createTaskListModel({
+	update,
+	remove: key => {
+		item(key).remove();
+		updateCount();
+	},
+
+	add: (key, value) => {
+		const element = list.querySelector('template').content.cloneNode(true).firstElementChild
+		element.id = `task-${key}`;
+		const form = element.querySelector('form');
+		form.elements.completed.addEventListener('change', e =>
+			model.updateTask(key, formValues(e.target.form)));
+
+		form.querySelector('label').addEventListener('dblclick', () => {
+			updateEditing(element);
+			element.querySelector('.edit').focus();
+		});
+
+		form.addEventListener('submit', e => {
+			if (e.submitter.name === 'destroy')
+				model.deleteTask(key);
+			else
+				model.updateTask(key, formValues(e.target));
+			updateEditing(null);
+			e.preventDefault();
+		});
+
+		element.querySelector('.edit').addEventListener('blur', () => updateEditing(null));
+		update(key, value, element);
+		list.appendChild(element);
+		updateCount();
+	},
+});
+
+mainForm.elements.clearCompleted.addEventListener('click', () => model.deleteCompleted());
+mainForm.elements.toggleAll.addEventListener('change', e => model.toggleAll(e.target.checked));
+mainForm.addEventListener('submit', e => {
+	model.createTask(formValues(e.target));
+	e.preventDefault();
+});
