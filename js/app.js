73dabef2ffcff34e919fb5f751affe63015ce2a9
Noam Rosenthal <noam@webkit.org>
2021-12-15 12:08:58 +0200

    Swish things around in JS

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
 import TaskListModel from './model.js';
 
-const list = document.querySelector('.todo-list');
-const {completedCount, totalCount, activeCount, clearCompleted, toggleAll, filter} = document.forms.main.elements;
-const updateFilter = () => filter.value = location.hash.substr(2);
-window.addEventListener('hashchange', updateFilter);
-window.addEventListener('load', updateFilter);
-document.addEventListener('submit', e => e.preventDefault(), {capture: true});
-
 const model = new TaskListModel(new class {
 	onAdd(key, value) {
-		const form = list.querySelector('template').content.cloneNode(true).firstElementChild;
+		const form = document.querySelector('.todo-list template').content.cloneNode(true).firstElementChild;
 		form.name = `task-${key}`;
 		form.elements.completed.onchange = form.onsubmit = () => model.updateTask(key,  Object.fromEntries(new FormData(form)));
 		form.elements.title.addEventListener('dblclick', ({target}) => target.removeAttribute('readonly'));
 		form.elements.title.addEventListener('blur', ({target}) => target.setAttribute('readonly', ''));
 		form.elements.destroy.addEventListener('click', () => model.deleteTask(key));
 		this.onUpdate(key, value, form);
-		list.appendChild(form);
+		document.querySelector('.todo-list').appendChild(form);
 	}
 
 	onUpdate(key, {title, completed}, form = document.forms[`task-${key}`]) {
@@ -27,13 +20,17 @@ const model = new TaskListModel(new class {
 
 	onRemove(key) { document.forms[`task-${key}`].remove(); }
 	onCountChange({active, completed}) {
-		completedCount.value = completed;
-		toggleAll.checked = active === 0;
-		totalCount.value = active + completed;
-		activeCount.innerHTML = `<strong>${active}</strong> item${active === 1 ? '' : 's'} left`;
+		document.forms.main.elements.completedCount.value = completed;
+		document.forms.main.elements.toggleAll.checked = active === 0;
+		document.forms.main.elements.totalCount.value = active + completed;
+		document.forms.main.elements.activeCount.innerHTML = `<strong>${active}</strong> item${active === 1 ? '' : 's'} left`;
 	}
 });
 
+const updateFilter = () => document.forms.main.elements.filter.setAttribute('value', location.hash.substr(2));
+window.addEventListener('hashchange', updateFilter);
+window.addEventListener('load', updateFilter);
+document.querySelector('.todoapp').addEventListener('submit', e => e.preventDefault(), {capture: true});
 document.forms.new.addEventListener('submit', ({target: {elements: {title}}}) => model.createTask({title}));
-toggleAll.addEventListener('change', ({target: {checked}})=> model.markAll(checked));
-clearCompleted.addEventListener('click', () => model.clearCompleted());
+document.forms.main.elements.toggleAll.addEventListener('change', ({target: {checked}})=> model.markAll(checked));
+document.forms.main.elements.clearCompleted.addEventListener('click', () => model.clearCompleted());
